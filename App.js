aee3a2ef5aa82ccb7a8132a86e9e2c0a60bc9e7c
Yuto Suzuki <yuutoo.advance@gmail.com>
2017-11-26 02:58:46 +0900

    fix component

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import {
   StyleSheet,
   Text,
   View,
+  FlatList,
+  Alert,
   Image
 } from 'react-native';
 
@@ -20,7 +22,15 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component {
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Demo extends Component {
   render() {
     let pic = {
       uri: 'https://nekogazou.com/wp-content/uploads/2015/10/4ab5442a6d977922bcbe8850ff4b40bc.jpg'
@@ -37,34 +47,54 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Greeting name='Yuto' />
         <Image source={pic} style={{width: 193, height: 110}}/>
       </View>
     );
   }
 }
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {showText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : ' ';
+class Todo extends Component {
+  render(){
     return (
-      <Text>{display}</Text>
+      <View>
+        <Text style={styles.title}>
+          Todo App
+        </Text>
+        <TodoList />
+      </View>
+    )
+  }
+}
+
+class TodoList extends Component {
+  _onPressButton() {
+    Alert.alert('終わった！！')
+  }
+  render() {
+    return (
+      <FlatList
+      data = {[{"key":"皿洗い"},{"key":"ゴミ捨て"}]}
+      renderItem={({item}) => <Text style={styles.item} onPress={this._onPressButton}>{item.key}</Text>}
+      />
+    )
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <Todo />
     );
   }
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
